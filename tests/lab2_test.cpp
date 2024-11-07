b0f738ead1af783f8198d2d46401936918d036e4
dmitriikuleshov <dmkuleshov10@gmail.com>
2024-11-08 01:14:42 +0300

    lab2: added necessary tests

--- tests/lab2_test.cpp
+++ tests/lab2_test.cpp
@@ -8,8 +8,31 @@ const std::string LAB2_PATH_TO_PROGRAMM = std::string(getenv("LAB2_PATH_TO_PROGR
 const std::string LAB2_PATH_TO_INPUT_DATA = std::string(getenv("LAB2_PATH_TO_INPUT_DATA"));
 const std::string LAB2_PATH_TO_OUTPUT_DATA = std::string(getenv("LAB2_PATH_TO_OUTPUT_DATA"));
 
+void ExecuteProgramm(const std::string command, std::string &output) {
+    std::array<char, 128> buffer;
+    FILE *pipe = popen(command.c_str(), "r");
+    if (!pipe) {
+        throw std::runtime_error("Can not open pipe!");
+    }
+    while (fgets(buffer.data(), buffer.size(), pipe) != nullptr) {
+        output += buffer.data();
+    }
+    pclose(pipe);
+}
 
-TEST(TestUsingDataFromFiles, TestMain) {
+void PutDataToTempFile(const std::string filePath, const std::string data) {
+    std::ofstream file(filePath);
+    if (!file.is_open()) {
+        std::cerr << "Can not open file for writing current points"
+                  << std::endl;
+        exit(1);
+    } else {
+        file << data;
+        file.close();
+    }
+}
+
+TEST(TestUsingDataFromFiles, TestUsingDataFromFiles) {
     std::string maxThreads = "10";
     std::ifstream inputDataFile(LAB2_PATH_TO_INPUT_DATA);
     std::ifstream outputDataFile(LAB2_PATH_TO_OUTPUT_DATA);
@@ -20,7 +43,6 @@ TEST(TestUsingDataFromFiles, TestMain) {
     std::string currentInputFilePath =
         std::filesystem::current_path().string() + "current_input_file.txt";
     std::string command;
-    std::array<char, 128> buffer;
 
     if (!(inputDataFile.is_open() && outputDataFile.is_open())) {
         std::cerr << "Can not open file with input data or output data"
@@ -49,35 +71,58 @@ TEST(TestUsingDataFromFiles, TestMain) {
         }
 
         if (inputData.empty() || outputData.empty()) {
-            exit(0);
+            inputDataFile.close();
+            outputDataFile.close();
+            std::remove(currentInputFilePath.c_str());
+            return;
         }
 
-        std::ofstream currentInputFile(currentInputFilePath);
-        if (!currentInputFile.is_open()) {
-            std::cerr << "Can not open file for writing current points"
-                      << std::endl;
-            exit(1);
-        } else {
-            currentInputFile << inputData;
-            currentInputFile.close();
-        }
+        PutDataToTempFile(currentInputFilePath, inputData);
 
         command = LAB2_PATH_TO_PROGRAMM + " " + currentInputFilePath + " " +
                   maxThreads;
-        FILE *pipe = popen(command.c_str(), "r");
-        if (!pipe) {
-            throw std::runtime_error("Can not open pipe!");
-        }
-        while (fgets(buffer.data(), buffer.size(), pipe) != nullptr) {
-            output += buffer.data();
-        }
-        pclose(pipe);
+        ExecuteProgramm(command, output);
 
         EXPECT_EQ(output, outputData);
     }
 
     inputDataFile.close();
     outputDataFile.close();
+    std::remove(currentInputFilePath.c_str());
+}
+
+TEST(OneThread, OneThread) {
+    std::string currentInputFilePath =
+        std::filesystem::current_path().string() + "current_input_file.txt";
+    std::string inputData = "0 0 2\n2 0 0\n0 0 2\n0 0 1\n0 1 0\n1 0 1";
+    PutDataToTempFile(currentInputFilePath, inputData);
+
+    std::string command =
+        LAB2_PATH_TO_PROGRAMM + " " + currentInputFilePath + " " + "1";
+    std::string outputOne;
+    ExecuteProgramm(command, outputOne);
+
+    std::string expectedOutput = "Point 1: (0.00, 0.00, 2.00)\nPoint 2: (2.00, 0.00, 0.00)\nPoint 3: (0.00, 1.00, 0.00)\nMaximum area: 2.45\n";
+    EXPECT_EQ(outputOne, expectedOutput);
+}
+
+
+TEST(TestTheSameResult, TestTheSameResult) {
+    std::string currentInputFilePath =
+        std::filesystem::current_path().string() + "current_input_file.txt";
+    std::string inputData = "0 0 2\n2 0 0\n0 0 2\n0 0 1\n0 1 0\n1 0 1";
+    PutDataToTempFile(currentInputFilePath, inputData);
+
+    std::string command1 =
+            LAB2_PATH_TO_PROGRAMM + " " + currentInputFilePath + " " + "1";
+    std::string command2 =
+        LAB2_PATH_TO_PROGRAMM + " " + currentInputFilePath + " " + "10";
+
+    std::string outputOne;
+    std::string outputMany;
+    ExecuteProgramm(command1, outputOne);
+    ExecuteProgramm(command2, outputMany);
+    EXPECT_EQ(outputOne, outputMany);
     std::remove(currentInputFilePath.c_str());
 }
 
