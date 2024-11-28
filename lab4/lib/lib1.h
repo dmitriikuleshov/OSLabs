68d75fd762dd4a0f85d3e6f214ddbfcac64a15af
dmitriikuleshov <dmkuleshov10@gmail.com>
2024-11-29 01:28:28 +0300

    lab4: corrected ifndef in .h files

--- lab4/lib/lib1.h
+++ lab4/lib/lib1.h
@@ -1,8 +1,8 @@
-#ifndef LIB1_C
-#define LIB1_C
+#ifndef LIB1_H
+#define LIB1_H
 #include <math.h>
 
 float FloatDerivative(float A, float deltaX);
 void BubbleSort(int *array, int size);
 
-#endif
+#endif // LIB2_H
--- lab4/lib/lib2.h
+++ lab4/lib/lib2.h
@@ -1,8 +1,8 @@
-#ifndef LIB2_C
-#define LIB2_C
+#ifndef LIB2_H
+#define LIB2_H
 #include <math.h>
 
 float FloatDerivative(float A, float deltaX);
 void QuickSort(int *array, int size);
 
-#endif
+#endif // LIB2_H
